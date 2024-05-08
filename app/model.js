87193567d23b86bd1c37a54555992089978e80e8
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-05-08 19:11:37 -0400

    made the site one page

--- app/model.js
+++ app/model.js
@@ -1,13 +1,13 @@
 export function changePage(pageID, callback) {
-    if (pageID == '' || pageID == "home") {
-        $.get(`pages/home.html`, function (contents) {
-            $('#app').html(contents);
-            console.log(pageID);
-        });
-    } else {
-        $.get(`pages/${pageID}.html`, function (contents) {
-            $('#app').html(contents);
-            console.log(pageID);
-        });
-    }
+  if (pageID == "" || pageID == "home") {
+    $.get(`pages/home.html`, function (contents) {
+      $("#app").html(contents);
+      console.log(pageID);
+    });
+  } else {
+    $.get(`pages/${pageID}.html`, function (contents) {
+      $("#app").html(contents);
+      console.log(pageID);
+    });
+  }
 }
