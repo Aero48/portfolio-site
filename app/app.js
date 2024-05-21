9de73af29b9c4b6ea395fdabc898f8bf424c083a
Aero48 <56200404+Aero48@users.noreply.github.com>
2024-05-21 11:29:32 -0400

    site is now responsive

--- app/app.js
+++ app/app.js
@@ -14,6 +14,17 @@ function initURLListener() {
   changeRoute();
 }
 
+function toggleHamburger() {
+  if ($("#hamburger-menu").css("top") == "60px") {
+    //$("#hamburger-menu").css("visibility", "hidden");
+    $("#hamburger-menu").css("top", "-150px");
+  } else {
+    //$("#hamburger-menu").css("visibility", "visible");
+    $("#hamburger-menu").css("top", "60px");
+  }
+}
+
 $(document).ready(function () {
   //initURLListener();
+  $("#hamburger-button").on("click", toggleHamburger);
 });
